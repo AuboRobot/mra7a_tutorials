7fc4b34c7fc64a4e6247a9b469377285a39b4367
lmn <mengnanli@aliyun.com>
2017-05-04 09:27:31 +0800

    marker_control_module.py: initial_pose={}

--- brain_control/marker_control_python/src/marker_control_python/marker_control_module.py
+++ brain_control/marker_control_python/src/marker_control_python/marker_control_module.py
@@ -90,84 +90,6 @@ class JoyStatus():
         self.right_analog_x = 0.0
         self.right_analog_y = 0.0
 
-class PS3Status(JoyStatus):
-    def __init__(self, msg):
-        JoyStatus.__init__(self)
-        # creating from sensor_msgs/Joy
-        if msg.buttons[16] == 1:
-            self.center = True
-        else:
-            self.center = False
-        if msg.buttons[0] == 1:
-            self.select = True
-        else:
-            self.select = False
-        if msg.buttons[3] == 1:
-            self.start = True
-        else:
-            self.start = False
-        if msg.buttons[1] == 1:
-            self.L3 = True
-        else:
-            self.L3 = False
-        if msg.buttons[2] == 1:
-            self.R3 = True
-        else:
-            self.R3 = False
-        if msg.axes[15] < 0:
-            self.square = True
-        else:
-            self.square = False
-        if msg.axes[4] < 0:
-            self.up = True
-        else:
-            self.up = False
-        if msg.axes[6] < 0:
-            self.down = True
-        else:
-            self.down = False
-        if msg.axes[7] < 0:
-            self.left = True
-        else:
-            self.left = False
-        if msg.axes[5] < 0:
-            self.right = True
-        else:
-            self.right = False
-        if msg.axes[12] < 0:
-            self.triangle = True
-        else:
-            self.triangle = False
-        if msg.axes[14] < 0:
-            self.cross = True
-        else:
-            self.cross = False
-        if msg.axes[13] < 0:
-            self.circle = True
-        else:
-            self.circle = False
-        if msg.axes[10] < 0:
-            self.L1 = True
-        else:
-            self.L1 = False
-        if msg.axes[11] < 0:
-            self.R1 = True
-        else:
-            self.R1 = False
-        if msg.axes[8] < 0:
-            self.L2 = True
-        else:
-            self.L2 = False
-        if msg.axes[9] < 0:
-            self.R2 = True
-        else:
-            self.R2 = False
-        self.left_analog_x = msg.axes[0]
-        self.left_analog_y = msg.axes[1]
-        self.right_analog_x = msg.axes[2]
-        self.right_analog_y = msg.axes[3]
-        self.orig_msg = msg
-
 class PS3WiredStatus(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
@@ -462,8 +384,6 @@ class MoveitJoy:
     def joyCB(self, msg):
         if len(msg.axes) == 27 and len(msg.buttons) == 19:
             status = PS3WiredStatus(msg)
-        elif len(msg.axes) == 20 and len(msg.buttons) == 17:
-            status = PS3Status(msg)
         else:
             raise Exception("Unknown joystick")
         self.run(status)
@@ -557,6 +477,7 @@ class MoveitJoy:
     def run(self, status):
         if self.history.new(status, "start"):
             self.initialized = False
+            self.initial_poses = {}
         if not self.initialized:
             # when not initialized, we will force to change planning_group
             while True:
