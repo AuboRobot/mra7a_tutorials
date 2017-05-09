51c13f55abb3dbcc4b9e798104a1ede2d6ba55b8
lmn <mengnanli@aliyun.com>
2017-05-09 09:51:21 +0800

    add the mra7a_random_motion to mra7a_tutorials

---
+++ mra7a_random_motion/src/main.cpp
@@ -0,0 +1,26 @@
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mra7a_random_motion");
+    ros::NodeHandle node_handle;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    moveit::planning_interface::MoveGroup group("arm");
+
+    while(ros::ok()){
+        group.setRandomTarget();
+
+        moveit::planning_interface::MoveGroup::Plan my_plan;
+        bool success = group.plan(my_plan);
+        ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+
+        group.execute(my_plan);
+
+        sleep(2);
+    }
+
+    return 0;
+}
